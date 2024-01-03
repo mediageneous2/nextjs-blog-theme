1bc1667c652addabdcb3f592e322be90dc231ba7
mediageneous2 <155568318+mediageneous2@users.noreply.github.com>
2024-01-03 23:26:13 +0300

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -52,7 +52,7 @@ export default function Layout({ children }) {
   return (
     <div className="relative pb-24 overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
-        {children}
+        {}
       </div>
     </div>
   );
